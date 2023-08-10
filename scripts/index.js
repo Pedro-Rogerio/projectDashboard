ad74c7c0f63d75a09cd7b44b5b12a057cb0344ad
Pedro Rogério <ti.pedrorogerio@gmail.com>
2023-08-09 22:50:07 -0300

    ADICIONADO CSS, ASIDE, NAV, OVERVIEW E MONTHLY, INICIO DE CHARTJS

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,18 @@
-const debounce = function(func, wait, immediate) { //função debounce da Lodash, biblioteca para segurar a frequencia da animação.
-    let timeout;
-    return function(...args) {
-      const context = this;
-      const later = function () {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  };
+var ctx = document.getElementsByClassName("line-chart");
 
-const target = document.querySelectorAll('[data-anime]'); //Selecionar as propriedades que eu quero alterar
-const animationClass = 'animate';
+/*config*/
 
-function animeScroll() {
-    const windowTop = window.scrollY + ((window.innerHeight * 3)/4);//Garantir certa responsividade, de modo a calcular 3/4 da altura de tela para iniciar a animação
-    target.forEach(function(e){
-        if ((windowTop) > (e.offsetTop)) {
-            e.classList.add(animationClass);
-            console.log("--");
-            console.log(window.scrollY);
-            console.log(window.innerHeight);
-            console.log(windowTop);
-            console.log(e.offsetTop);
-        }
-        else{
-            e.classList.remove(animationClass);
-        }
-    })
-}
-
-
-animeScroll(); //Garantindo que a função seja sempre ativada desde o momento que o usuario entrou no site.
-
-
-if(target.length) {//Esse comando avalia se tem algum target a ser animado na página, se não tiver ele não vai executar o comando, porque o leght vai trazer o resulta do 0 caso não tenha e por consequencia será negativo. Isso é bom para caso a pagina não precise desse código.
-    window.addEventListener('scroll', debounce(function() {
-      animeScroll();
-    }, 5));
+var chartGraph = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "Jun","Jul"],
+    datasets:[{
+      label: "GASTOS",
+      data: [2560,1250,1866,2599,1587,2311],
+      borderWidth: 6,
+      borderColor: 'rgba(88, 0, 237, 0.85)',
+      backgroundColor: 'trasparent',
+    }]
   }
+ 
+});
