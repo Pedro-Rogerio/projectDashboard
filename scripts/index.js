985650b32e3b296d7207cb8d598bc1e99a725ddc
Pedro Rogério <120759586+Pedro-Rogerio@users.noreply.github.com>
2023-08-10 14:45:41 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,27 @@ var chartGraph = new Chart(ctx, {
   data: {
     labels: ["Jan", "Feb", "Mar", "Apr", "Jun","Jul"],
     datasets:[{
-      label: "GASTOS",
+      label: "Inter",
+      data: [440,153,268,900,1250,843],
+      borderWidth: 6,
+      borderColor: 'rgba(255, 146, 0, 0.8)',
+      backgroundColor: 'trasparent',
+    },
+    {
+      label: "Amex",
+      data: [3002,2234,3212,2988,2333,2456],
+      borderWidth: 6,
+      borderColor: 'rgba(50, 50, 50, 0.8)',
+      backgroundColor: 'trasparent',
+    }, 
+    {
+      label: "Nubank",
       data: [2560,1250,1866,2599,1587,2311],
       borderWidth: 6,
       borderColor: 'rgba(88, 0, 237, 0.85)',
       backgroundColor: 'trasparent',
     }]
-  }
+  },
+
  
 });
